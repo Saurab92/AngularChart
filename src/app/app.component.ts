d4b3757919d46046c1972e1099726d1f4a8a48d0
Saurabh y <saurabhy@hcl.com>
2019-10-22 14:55:09 +0530

    Creating Chart for the corresponding json data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Chart } from 'chart.js';
+import { Data } from './Data';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,45 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'chart';
+  data: Data[];
+  url = 'http://localhost:4000/results';
+  month = [];
+  price = [];
+  chart = [];
+  constructor(private httpClient: HttpClient) {}
+
+  ngOnInit() {
+    this.httpClient.get(this.url).subscribe((res: Data[]) => {
+      res.forEach(y => {
+        this.month.push(y.month);
+        this.price.push(y.price);
+      });
+      this.chart = new Chart('canvas', {
+        type: 'line',
+        data: {
+          labels: this.month,
+          datasets: [
+            {
+              data: this.price,
+              borderColor: '#3cba9f',
+              fill: false
+            }
+          ]
+        },
+        options: {
+          legend: {
+            display: false
+          },
+          scales: {
+            xAxes: [{
+              display: true
+            }],
+            yAxes: [{
+              display: true
+            }],
+          }
+        }
+      });
+    });
+  }
 }
